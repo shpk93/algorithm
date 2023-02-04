ef28df0de6c998fb71a398df4c644c4f4c3a3877
shpk93 <shpk930924@gmail.com>
2023-02-05 07:10:22 +1200

    15655

---
+++ 15655/15655.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const splitStr = process.platform === "linux" ? "\n" : "\r\n";
+var input = fs.readFileSync(filepath).toString().split(splitStr);
+
+const [N, M] = input[0].split(" ").map(Number);
+const nums = input[1].split(" ").map(Number);
+function solution(N, M, nums) {
+  nums.sort((a, b) => a - b);
+  let answer = [];
+
+  function rec(index, arr) {
+    if (arr.length === M) {
+      answer.push(arr.join(" "));
+      return;
+    }
+
+    for (let i = index; i < nums.length; i++) {
+      arr.push(nums[i]);
+      rec(i + 1, arr);
+      arr.pop();
+    }
+  }
+  rec(0, []);
+  return answer.join("\n");
+}
+
+console.log(solution(N, M, nums));
