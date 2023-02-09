e4f12010ade5e7c230e711c8b75b86ea7efb44da
shpk93 <shpk930924@gmail.com>
2023-02-10 05:11:27 +1000

    14956

---
+++ 14956/14956.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const splitStr = process.platform === "linux" ? "\n" : "\r\n";
+var input = fs.readFileSync(filepath).toString().split(splitStr);
+function solution(input) {
+  console.log(input);
+}
+
+console.log(solution(input));
