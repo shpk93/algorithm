f4e3faf963ab622e300991d19e4dd4e5bb57f748
shpk93 <shpk930924@gmail.com>
2023-04-01 03:22:30 +0400

    11053

---
+++ 11053/11053.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+const filepath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const splitStr = process.platform === "linux" ? "\n" : "\r\n";
+var input = fs.readFileSync(filepath).toString().split(splitStr);
+const N = +input[0];
+const nums = input[1].split(" ").map(Number);
+
+function solution(N, nums) {
+  let dp = Array(nums.length).fill(1);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}
+
+console.log(solution(N, nums));
